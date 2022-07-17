31905122f6729606e13c6b5d5126272efc6c22a9
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2022-07-17 22:56:24 +0530

    setting the attributes(classname)

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0 //add value dynamically with state
+        count: 1, //add value dynamically with state
     }
     render() { 
-        return (
+        return (//setting the attributes(classname)
             <div>
-                <span>{this.state.count}</span> 
-                <button>increament</button>
+                <span className='badge badge-primary m-2'>{this.formatCouter()}</span> 
+                <button className='btn btn-secondary btn-sm'>increament</button>
             </div>
         );
 
+    }
+    formatCouter () {
+        const {count} = this.state;
+        return count === 0  ? "zero" : count;
     }
 }
  
