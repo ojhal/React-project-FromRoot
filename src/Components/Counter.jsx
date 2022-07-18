0886e9ed7ef029dd853106107884b59f060b94dd
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2022-07-18 10:22:55 +0530

    rendering classes dynamically and refactoring

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 1, //add value dynamically with state
+        count: 0 //add value dynamically with state
     }
     render() { 
+        // let classes = this.getBadgeClasses(); //Refactoring
         return (//setting the attributes(classname)
             <div>
-                <span className='badge badge-primary m-2'>{this.formatCouter()}</span> 
+                <span className={this.getBadgeClasses()}>{this.formatCouter()}</span> 
                 <button className='btn btn-secondary btn-sm'>increament</button>
             </div>
         );
 
     }
+    getBadgeClasses() {//rendering classes dynamically
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCouter () {
         const {count} = this.state;
         return count === 0  ? "zero" : count;
